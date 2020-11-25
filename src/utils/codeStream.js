6e2a0fb068c53651413b322ab55547eb4c6c1cea
Alexey Radziuk <radyukag@yandex.ru>
2020-11-25 12:00:34 +0300

    feat: add abc

--- src/utils/codeStream.js
+++ src/utils/codeStream.js
@@ -2,34 +2,10 @@ const stream = require('stream');
 const fs = require('fs');
 const process = require('process');
 
-function optionsCheck(options) {
-    if (options.input === undefined) {
-        throw new Error ('Input file does not exist');
-    }
-    if (options.output === undefined) {
-        throw new Error ('Output file does not exist');
-    }
-}
-
 function codeStream({ input, output, action, shift }) {
-    let streamInput;
-    let streamOutput;
-    if (!streamInput) {
-        streamInput = process.stdin;
-    } else {
-        if (!fs.existsSync(input)) {
-            
-        }
-        streamInput = fs.createReadStream(input);
-    }
-    if (!streamOutput) {
-        streamOutput = process.stdout;
-    } else {
-        if (!fs.existsSync(output)) {
-
-        }
-    }
-
+    const streamInput = fs.existsSync(input) ? fs.createReadStream(input) : process.stdin;
+    const streamOutput = fs.existsSync(output) ? fs.createWriteStream(output) : process.stdout;
+    const transform = 
 }
 
 module.exports = codeStream;
