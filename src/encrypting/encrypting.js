548c7300dad766a7c909d6c52a68d930e70eb7a1
Alexey Radziuk <radyukag@yandex.ru>
2020-11-27 11:28:55 +0300

    fix: remove extra code

--- src/encrypting/encrypting.js
+++ src/encrypting/encrypting.js
@@ -3,12 +3,14 @@ const abc = require('../data/data');
 function encode(shift) {
     return function (chunk) {
         const result = chunk.toString('utf-8').split('').map((el) => {
-            const idx = abc.indexOf(el);
+            const isUpperCase = el === el.toUpperCase();
+            const idx = abc.indexOf(el.toLowerCase());
             if (idx < 0) {
                 return el;
             }
             const newIdx = idx + shift;
-            return newIdx < abc.length ? abc[newIdx] : abc[newIdx - abc.length];
+            const encodeEl = newIdx < abc.length ? abc[newIdx] : abc[newIdx - abc.length];
+            return isUpperCase ? encodeEl.toUpperCase() : encodeEl;
         });
         return result.join('');
     }
@@ -17,11 +19,14 @@ function encode(shift) {
 function decode(shift) {
     return function (chunk) {
         const result = chunk.toString('utf-8').split('').map((el) => {
-            const idx = abc.indexOf(el);
+            const isUpperCase = el === el.toUpperCase();
+            const idx = abc.indexOf(el.toLowerCase());
             if (idx < 0) {
                 return el;
             }
-            return idx - shift >= 0 ? abc[idx + shift] : abc[abc.length - 1 - (shift - idx)];
+            const newIdx = idx - shift;
+            const decodeEl = newIdx >= 0 ? abc[newIdx] : abc[abc.length + newIdx];
+            return isUpperCase ? decodeEl.toUpperCase() : decodeEl;
         });
         return result.join('');
     }
